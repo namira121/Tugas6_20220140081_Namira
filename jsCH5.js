5744b58be3f48d76601eec60b7954c2b154e114c
namira121 <127211008+namira121@users.noreply.github.com>
2024-04-08 14:12:53 +0700

    first commit

---
+++ jsCH5.js
@@ -0,0 +1,12 @@
+video.addEventListener("click", function video(){
+    alert("ini MV Katy Perry-Chained To The Rythym");
+});
+
+audio.addEventListener("click",function audio(){
+    alert("ini lagu Katy Perry-Chained To The Rythym");
+});
+
+gambar.addEventListener("click", function gambar(){
+    alert("Ini gambar Katy Perry")
+});
+
